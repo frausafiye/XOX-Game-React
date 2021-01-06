2def86c5d650fb7789140ddc9fe61cc774ba33c0
frausafiye <s_gulenc@outlook.com>
2021-01-06 14:58:08 +0100

    Updates

---
+++ precache-manifest.d8307844d3464b9a23913221d8ce25af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43f86a8cd6461379e21edcfafaad1c4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cfbbd34bf2f609835885",
+    "url": "./static/css/main.0c18b158.chunk.css"
+  },
+  {
+    "revision": "ca55e1c9e7fc99ec031f",
+    "url": "./static/js/2.687f16c8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.687f16c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfbbd34bf2f609835885",
+    "url": "./static/js/main.ee55b535.chunk.js"
+  },
+  {
+    "revision": "8cdb25f3ffb41440c4d8",
+    "url": "./static/js/runtime-main.e6c4d886.js"
+  }
+]);
